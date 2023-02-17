4b7264289b2bad67030106f75d76de0757e4accd
Cokke <coke.jgomez@gmail.com>
2023-02-17 17:44:04 -0300

    Se agrega al controller la implementacion de logger

--- src/main/java/cl/cokke/PruebaGradleAPI3/controller/PersonaController.java
+++ src/main/java/cl/cokke/PruebaGradleAPI3/controller/PersonaController.java
@@ -3,6 +3,8 @@ package cl.cokke.PruebaGradleAPI3.controller;
 import cl.cokke.PruebaGradleAPI3.exception.BadRequestException;
 import cl.cokke.PruebaGradleAPI3.model.Persona;
 import cl.cokke.PruebaGradleAPI3.service.PersonaService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +22,7 @@ public class PersonaController {
     @Autowired
     PersonaService personaService;
 
+    private static final Logger log= LoggerFactory.getLogger(PersonaController.class);
     @GetMapping("personas")
     public ResponseEntity<?> buscarTodos(){
         List<Persona> personasList = personaService.findAll();
@@ -32,6 +35,7 @@ public class PersonaController {
         if (p == null) {
             throw new BadRequestException("Persona es requerida");
         }
+        log.info("Usuario creado "+ personaService.create(p));
         return new ResponseEntity<>(personaService.create(p), HttpStatus.CREATED);
     }
 }
