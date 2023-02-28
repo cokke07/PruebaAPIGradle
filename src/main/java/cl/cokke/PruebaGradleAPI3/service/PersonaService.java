b45101c49f8383f1d59374896f59f47df47b0030
Cokke <coke.jgomez@gmail.com>
2023-02-28 18:38:57 -0300

    Se agregan el CRUD completo con los metodos delete, buscar por Id, actualizar

--- src/main/java/cl/cokke/PruebaGradleAPI3/service/PersonaService.java
+++ src/main/java/cl/cokke/PruebaGradleAPI3/service/PersonaService.java
@@ -1,6 +1,7 @@
 package cl.cokke.PruebaGradleAPI3.service;
 
 import cl.cokke.PruebaGradleAPI3.model.Persona;
+import org.springframework.http.HttpStatus;
 
 import java.util.List;
 
@@ -8,4 +9,7 @@ public interface PersonaService {
 
     public List<Persona> findAll();
     public Persona create(Persona p);
+    public Persona findByID(Long id);
+    public Persona updatePersona(Persona p);
+    public HttpStatus deletePersonaPorId(Long id);
 }
--- src/main/java/cl/cokke/PruebaGradleAPI3/service/PersonaServiceImpl.java
+++ src/main/java/cl/cokke/PruebaGradleAPI3/service/PersonaServiceImpl.java
@@ -4,6 +4,7 @@ import cl.cokke.PruebaGradleAPI3.model.Persona;
 import cl.cokke.PruebaGradleAPI3.repository.PersonaRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -22,5 +23,22 @@ public class PersonaServiceImpl implements PersonaService{
     public Persona create(Persona p) {
 
         return personaRepository.save(p);
+    }
+
+    @Override
+    public Persona findByID(Long id) {
+        return personaRepository.findById(id).orElseThrow();
+    }
+
+    @Override
+    public Persona updatePersona(Persona p) {
+        return personaRepository.save(p);
+    }
+
+    @Override
+    public HttpStatus deletePersonaPorId(Long id) {
+        personaRepository.deleteById(id);
+
+        return HttpStatus.OK;
     }
 }
